90f086c83bf5947b8f43c1f403dbfe429e270146
saad hafa <saad.811@icloud.com>
2019-06-06 17:49:56 +0000

    added register and post rout and get rout plus validation email and password

--- express_server.js
+++ express_server.js
@@ -5,12 +5,18 @@ const cookieParser = require('cookie-parser');
 // const bodyParser = require("body-parser");
 // app.use(bodyParser.urlencoded({extended: true}));
 // this is working its built in express dont need to install body-parser
+
+// midleware
+
 app.use(express.json());
+
 app.use(express.urlencoded({
   extended: false
-}))
+}));
 
-app.use(cookieParser())
+app.use(cookieParser());
+
+// set view engine 
 
 app.set('view engine', 'ejs');
 
@@ -18,17 +24,61 @@ let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
-
-app.post('/login',(req,res) =>{
-    const { username } = req.body;
-    res.cookie('username', username);
-    res.redirect('/')
-})
+// My functions 
 
 function generateRandomString() {
   return (Math.random() * 6).toString(36).substring(6).toUpperCase();
 }
+
+
+function newUserCheck(email, password){
+  if(email !== '' && password !== '' ){
+      return true;
+  }
+  return false;
+}
+
+
+// post request
+
+// app.post('/login',(req,res) =>{
+//     const { username } = req.body;
+//     res.cookie('user_id', username);
+//     res.redirect('/')
+// });
+
+app.post('/register',(req,res)=>{
+  const { email, password } = req.body;
+  const userId = generateRandomString();
+  if(newUserCheck(email, password)){
+    users[userId]  = {
+      id: userId,
+      email: email,
+      password: password
+    }
+    console.log('user_id',userId, users)
+      if(users[userId]) {
+        res.cookie('user_id',userId);
+        res.status(200).redirect('/')
+      }
+  }
+    res.status(401).redirect('/register');
+});
+
+
 app.post("/urls", (req, res) => {
   let random = generateRandomString().toString();
   urlDatabase[random] = req.body.longURL;
@@ -49,10 +99,11 @@ app.post('/urls/:shortURL/update', (req, res) => {
 });
 
 app.post('/logout', (req,res)=>{
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/');
 });
 
+// get requests 
 
 app.get("/u/:shortURL", (req, res) => {
   // const longURL = ... 
@@ -62,26 +113,30 @@ app.get("/u/:shortURL", (req, res) => {
   } else {
     res.send('error')
   }
-  // res.redirect(longURL);
 });
 
 
-// console.log(generateRandomString())
+app.get('/register', (req,res) =>{
+  const username = { cookie: req.cookies['user_id'], users: users }
+  res.render('register', username);
+})
+
 app.get("/", (req, res) => {
-  let templateVars = {urls: urlDatabase,username: req.cookies['username']};
+  let templateVars = {urls: urlDatabase,cookie: req.cookies['user_id'], users:users};
   res.render("urls_index", templateVars);
 });
 
 
 app.get('/urls/new', (req, res) => {
-  res.render("urls_new",{username: req.cookies['username']});
+  res.render("urls_new",{cookie: req.cookies['user_id'], users:users});
 })
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies['username']
+    cookie: req.cookies['user_id'],
+    users:users
   };
   res.render("urls_show", templateVars);
 });
